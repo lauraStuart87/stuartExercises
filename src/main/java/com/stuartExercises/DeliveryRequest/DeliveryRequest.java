64a3dc112050b70486a1c1f2cc452d3689cc7bca
lhirsch <laura.hirsch@trivago.com>
2018-06-02 14:23:35 +0200

    add test for checking prices

--- src/main/java/com/stuartExercises/DeliveryRequest/DeliveryRequest.java
+++ src/main/java/com/stuartExercises/DeliveryRequest/DeliveryRequest.java
@@ -2,7 +2,6 @@ package com.stuartExercises.DeliveryRequest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,8 +20,12 @@ public class DeliveryRequest {
     private static final String DROP_OFF_ADDRESS_FIELD_ID = "dropOffCard-0-fields-field-address";
     private static final String DROP_OFF_ADDRESS = "Avinguda Diagonal 64";
     private static final String REQUEST_DELIVERY_BTN_ID = "requestButton";
-    private static final String LAST_ACTIVE_JOB_PAGE_CLASS= "_1hvvIGew";
-    private static final String REQUEST_DELIVERY_PARENT = "_3DvsBvhT";
+    private static final String IN_PROGRESS_PAGE_PARENT_CLASS = "_1hvvIGew";
+    private static final String LAST_ACTIVE_JOB_CARD_CLASS = "_3qv1HTOr";
+    private static final String REQUEST_DELIVERY_PARENT_CLASS = "_3DvsBvhT";
+    private static final String TRANSPORTATION_TYPE_BIKE = "icon-bike";
+    private static final String TRANSPORTATION_CARD_PARENT_ID = "vehicleCard";
+    private static final String TRANSPORTATIONS_PRICES_CLASS = "_2GUaVkNp";
 
     private WebElement getPickUpCardParent(){
         return driver.findElement(By.id(PICK_UP_CARD_ID));
@@ -33,7 +36,15 @@ public class DeliveryRequest {
     }
 
     private WebElement getRequestDeliveryBtnParent(){
-        return driver.findElement(By.className(REQUEST_DELIVERY_PARENT));
+        return driver.findElement(By.className(REQUEST_DELIVERY_PARENT_CLASS));
+    }
+
+    private WebElement getLastAciveJobParent(){
+        return driver.findElement(By.className(LAST_ACTIVE_JOB_CARD_CLASS));
+    }
+
+    private WebElement getTrasportationCardParent(){
+        return driver.findElement(By.id(TRANSPORTATION_CARD_PARENT_ID));
     }
 
     public String getPickUpCardTitle() {
@@ -50,6 +61,11 @@ public class DeliveryRequest {
     private void waitForClassToBeVisible(String className){
         WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(className)));
+    }
+
+    private void waitForElementToByClickableByLocator(String locator){
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(locator)));
     }
 
     public void userFillsPickUpField() {
@@ -73,16 +89,26 @@ public class DeliveryRequest {
     }
 
     public void userRequestsDelivery() {
-        //waitForIdToBeVisible(REQUEST_DELIVERY_BTN_ID);
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.elementToBeClickable(By.id(REQUEST_DELIVERY_BTN_ID)));
+        waitForElementToByClickableByLocator(REQUEST_DELIVERY_BTN_ID);
         WebElement requestDeliveryBtn = getRequestDeliveryBtnParent().findElement(By.id(REQUEST_DELIVERY_BTN_ID));
         requestDeliveryBtn.click();
     }
 
     public String getLastActiveJobAddresses() {
-        waitForClassToBeVisible(LAST_ACTIVE_JOB_PAGE_CLASS);
-        WebElement activeJob = driver.findElement(By.className(LAST_ACTIVE_JOB_PAGE_CLASS));
+        waitForClassToBeVisible(IN_PROGRESS_PAGE_PARENT_CLASS);
+        WebElement activeJob = getLastAciveJobParent();
         return activeJob.getText();
+    }
+
+    public boolean getBikeTransportType() {
+        waitForClassToBeVisible(TRANSPORTATION_TYPE_BIKE);
+        WebElement bike = getLastAciveJobParent().findElement(By.className(TRANSPORTATION_TYPE_BIKE));
+        return bike.isDisplayed();
+    }
+
+    public int getTransportPrices() {
+        waitForClassToBeVisible(TRANSPORTATIONS_PRICES_CLASS);
+        List<WebElement> vehiclesPrices = getTrasportationCardParent().findElements(By.className(TRANSPORTATIONS_PRICES_CLASS));
+        return vehiclesPrices.size();
     }
 }
--- src/test/java/com/stuartExercises/Features/RequestDeliveryTest.java
+++ src/test/java/com/stuartExercises/Features/RequestDeliveryTest.java
@@ -4,7 +4,10 @@ import com.stuartExercises.DeliveryRequest.DeliveryRequest;
 import com.stuartExercises.Login.Login;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class RequestDeliveryTest {
@@ -13,15 +16,28 @@ public class RequestDeliveryTest {
     private DeliveryRequest deliveryRequest = new DeliveryRequest();
 
     @Test
-    public void userIsAbleToRequestAnInmediateDeliveryByCar(){
+    public void userIsAbleToRequestAnInmediateDelivery(){
         //We log in
         logIn.userLogsIn();
         //We add the pick up and drop off addresses and the type of transport
         deliveryRequest.userFillsPickUpField();
         deliveryRequest.userFillsDestinationField();
         deliveryRequest.userRequestsDelivery();
-        //In the "In Progress" tab we check that the data of the delivery is correct
+        //We check we are inn the "In Progress" tab, and that the data of the delivery is correct
         assertThat(deliveryRequest.getLastActiveJobAddresses(), containsString("Avinguda Diagonal 5"));
         assertThat(deliveryRequest.getLastActiveJobAddresses(), containsString("Avinguda Diagonal 64"));
+        assertThat(deliveryRequest.getBikeTransportType(), is(true));
     }
+
+    @Test
+    public void userIsAbleToSeePricesAfterAddingPickUpAndDropOffAddresses(){
+        //We log in
+        logIn.userLogsIn();
+        //We add the pick up and drop off addresses
+        deliveryRequest.userFillsPickUpField();
+        deliveryRequest.userFillsDestinationField();
+        //We check that the prices for all 4 transport types are available for the user
+        assertEquals(deliveryRequest.getTransportPrices(), 4);
+    }
+
 }
